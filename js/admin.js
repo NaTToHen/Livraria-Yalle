411b181684171ec953ea3cc45738cd15a76b4210
Henrique Bonatto <nattohen@gmail.com>
2023-04-11 19:33:16 -0300

    navegação por tabs

---
+++ js/admin.js
@@ -0,0 +1,21 @@
+function iniciarTab() {
+   const menu = document.querySelectorAll(".listaMenu li");
+   const conteudo = document.querySelectorAll("#conteudo section");
+
+   if(menu.length && conteudo.length) {
+      conteudo[0].classList.add("active");
+
+      function activeTab(index) {
+         conteudo.forEach((section) => {
+            section.classList.remove("active");
+         });
+         conteudo[index].classList.add("active");
+      }
+      menu.forEach((itemMenu, index) => {
+         itemMenu.addEventListener("click", () => {
+            activeTab(index);
+         });
+      });
+   }
+}
+iniciarTab();
