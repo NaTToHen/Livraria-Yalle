0a4121ee6c481982d8ddd8f895d2d926a372dfbe
Henrique Bonatto <nattohen@gmail.com>
2023-04-19 19:15:25 -0300

    add conexao.php

--- lib/conexao.php
+++ lib/conexao.php
@@ -2,7 +2,7 @@
 error_reporting();
 
 $conn = new misqli("localhost", "root", "", "");
-if($conn ->connect_errno) {
+if($conn -> connect_errno) {
    echo "falha ao se conectar com o servidor".$misqli -> connect_errno;
 }
 ?>
